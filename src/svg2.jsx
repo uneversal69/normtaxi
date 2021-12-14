59df9120d93ab3a25a1ca1630e495b24f7d831b8
uneversal69 <uneversal69@yandex.ru>
2021-12-14 12:45:01 +0300

    go new2

---
+++ src/svg2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function svg1(){
+    return (<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="6" cy="6" r="6" fill="#DDDDDD"/>
+    </svg>
+    )
+}
+
+export default svg1;
